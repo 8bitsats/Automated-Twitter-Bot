7cf8c6c68249385b68290ad0a6555da376d7fcb7
ndarugaa <ndaruga.francis@gmail.com>
2023-08-15 19:47:54 +0300

    ---

--- telegram.py
+++ telegram.py
@@ -1,20 +1,27 @@
-import telegram
-from telegram.ext.chatbot import Chat
+import requests
+import os
 
+def download_media(channel_id, media_file_url):
+    """Downloads a media file from a Telegram channel.
 
-# def go_to_first_message(channel_id, date=None):
-#     if date is None:
-#         date = "01/01/2023"
+    Args:
+        channel_id: The ID of the Telegram channel.
+        media_file_url: The URL of the media file to download.
+    """
 
-#     chat = telegram.Chat(channel_id)
-#     messages = chat.get_messages(
-#         limit=chat.total_messages,
-#         date=date,
-#     )
+    filename = media_file_url.split("/")[-1]
+    response = requests.get(media_file_url, stream=True)
 
-#     if messages:
-#         first_message = messages[0]
-#         chat.scroll_to(first_message)
+    if response.status_code == 200:
+        with open(os.path.join("Media_Files", filename), "wb") as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                f.write(chunk)
 
-# go_to_first_message("@Nature")
+    else:
+        print("Error downloading media file:", response.status_code)
 
+if __name__ == "__main__":
+    channel_id = "Nature"
+    media_file_url = "https://t.me/Nature/1"
+
+    download_media(channel_id, media_file_url)
