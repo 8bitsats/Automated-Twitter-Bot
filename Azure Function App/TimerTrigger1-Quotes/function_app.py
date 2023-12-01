943d11966002cd312cfc6f3bc4099eeaa62132a6
ndarugaa <ndaruga.francis@gmail.com>
2023-12-01 15:32:23 +0300

    clean up and folder arrangements

--- Azure Function App/TimerTrigger1-Quotes/function_app.py
+++ Azure Function App/TimerTrigger1-Quotes/function_app.py
@@ -5,26 +5,28 @@ import logging
 import requests
 from io import BytesIO
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 import azure.functions as func
 from datetime import datetime, timezone
-from azure.storage.blob import BlobServiceClient, PublicAccess, BlobType
+from azure.storage.blob import BlobType
+from azure.storage.blob import PublicAccess
+from azure.storage.blob import BlobServiceClient
 
-load_dotenv()
 
-def main(mytimer: func.TimerRequest) -> None:
-	utc_timestamp = datetime.utcnow().replace(
-		tzinfo=timezone.utc).isoformat()
 
-	if mytimer.past_due:
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 */2 * * * *", arg_name="myTimer", run_on_startup=True,
+				use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+	if myTimer.past_due:
 		logging.info('The timer is past due!')
 
-#------------------------------ AUTHENTICATION -------------------------------------------#
+	#********************************Autentication**********************#
 
-#----------------- Authenticate Azure --------------------------------#
-	connection_string = "Your Connection String"
-	container_name = "Container Name"
-	blob_name = "Blob name" #Log file name. (.txt)
+	# Authenticate Azure
+	connection_string = "DefaultEndpointsProtocol=https;AccountName=newstuffstorageacc;AccountKey=4rMg0VGMoe4JZnH/wtYsIks3yzlswz2ZN/+WZUfWe+B3EDk5OAowfcA7XOS5/jtagWkIAktZxJE0+AStqAm5Qw==;EndpointSuffix=core.windows.net"
+	container_name = "motivationquotes"
+	blob_name = "logfile.txt"
 
 	blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
@@ -43,13 +45,14 @@ def main(mytimer: func.TimerRequest) -> None:
 		column_titles = "Time, Channel, Message_id, media_type, Caption, Tweet_ID"
 		blob_client.upload_blob(column_titles, blob_type=BlobType.BlockBlob)
 
-#--------------------Authenticate twitter------------------------------#
+	# authenticate twitter
 	try:
-		consumer_key = ""
-		consumer_secret = ""
-		access_token = ""
-		access_token_secret = ""
-		bearer_token = ""
+		consumer_key = "Uyo5A29Pvycb8c7IqAkSq47Vd"
+		consumer_secret = "9uNYNSSLOlZ0IvFIOhfGPTfVRv2T21dDKJqIrDgghAEZNPAFbl"
+		access_token = "1483479711510638601-rpEYVKnHKcQZ48xhcJf9Q1RuZYGf0U"
+		access_token_secret = "gmDZnI1zS28GnloaOgc8eaRii1hqGAcn3lgiZTZoOrAZi"
+		bearer_token = "AAAAAAAAAAAAAAAAAAAAAN0%2BnQEAAAAA3ciJTIEiMdqAdGhuTXB57sw%2FuPc%3DCqFW0IPYu7YgqajLAXAJ4NE9ywvPOZNVLyQHn3uVl5BiVoiBYJ"
+
 
 		auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
@@ -61,23 +64,22 @@ def main(mytimer: func.TimerRequest) -> None:
 
 		# V2 Twitter API Authentication
 		client = tweepy.Client(
-			bearer_token,
-			consumer_key,
-			consumer_secret,
-			access_token,
-			access_token_secret,
-			wait_on_rate_limit=True,
+        bearer_token,
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret,
+        wait_on_rate_limit=True,
 		)
 
 	except tweepy.TweepyException as e:
 		logging.error(f"Twitter authentication failed: {e}")
 		return None
 
-#************************************** VARIABLES **************************************************
-
+	#************************************ VARIABLES ****************************************************
 	log_values = []
-	channel_name = "CMK_Wallpapers"
-	message_id = random.choice(range(0,6500))
+	channel_name = "mqquotes"
+	message_id = random.choice(range(0,2345))
 	account_name = re.search("AccountName=(.*?);", connection_string).group(1)
 	telegram_download_url = f"https://t.me/{channel_name}/{message_id}?embed=1&mode=tme"
 	response = requests.get(url=telegram_download_url)
@@ -91,14 +93,16 @@ def main(mytimer: func.TimerRequest) -> None:
 	# Read the existing data from the blob
 	existing_data = blob_client.download_blob().readall() if blob_client.exists() else b""
 	existing_lines = existing_data.decode('utf-8').strip().split('\n')
+	logging.info(log_values)
 
 	numbers = []
 
-	# Skip the first line (assuming it contains column names)
+	# Skip the first line it contains column names
 	for string in existing_lines[1:]:
 		parts = string.split(',')
 		if len(parts) >= 3:
 			numbers.append(int(parts[2]))
+	logging.info(numbers)
 
 	# Check if the generated number exists in the existing data
 	if message_id in numbers:
@@ -138,18 +142,21 @@ def main(mytimer: func.TimerRequest) -> None:
 		# Create a BlobClient for the file in the container
 		filename = f'{channel_name}-{message_id}.jpg'
 		media_blob_client = blob_service_client.get_blob_client(container_name, filename)
+		logging.info('We are here')
 
 		# Upload the file's content to Azure Blob Storage
 		if not media_blob_client.exists():
 			media_blob_client.upload_blob(img_response.content)
 
 #--------------------- SAVE CAPTION TO LOG FILE-------------------#
-	# if bool(soup.find('div', class_='tgme_widget_message_text')) == True:
-	#     message_div = soup.find('div', class_='tgme_widget_message_text')
-	#     caption = list(message_div.stripped_strings)[0]  # This splits the content based on the presence of tags
-	# else:
-	#     # caption = "#Nature is just amazing 🌎"
-	caption = '' # Text to be Tweeted
+	if bool(soup.find('div', class_='tgme_widget_message_text')) == True:
+	    message_div = soup.find('div', class_='tgme_widget_message_text')
+	    caption = list(message_div.stripped_strings)[0]  # This splits the content based on the presence of tags
+	else:
+	    # caption = "#Nature is just amazing 🌎"
+	    caption = ""
+
+	# caption = '' # Text to be Tweeted
 
 	log_values.append(caption)
 
@@ -183,3 +190,6 @@ def main(mytimer: func.TimerRequest) -> None:
 
 	# Upload the updated data to the blob
 	blob_client.upload_blob(updated_data, blob_type=BlobType.BlockBlob, overwrite=True)
+
+	# display all log values
+	logging.info(log_values)
